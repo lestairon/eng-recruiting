ebde3719f833e8f56b7f16457a7285a63364d842
Lestairon <fercho092000@hotmail.com>
2021-09-03 22:36:29 -0500

    display table from API data (functional, needs refactor)

--- ims-bank/ruby/main.rb
+++ ims-bank/ruby/main.rb
@@ -1,11 +1,37 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'faraday'
+require 'faraday_middleware'
+require 'monetize'
 
-set :protection, :except => :frame_options
+set :protection, except: :frame_options
 set :bind, '0.0.0.0'
 set :port, 8080
+set :haml, { escape_html: false }
 
 get '/' do
-  # Your implementation goes here
-  erb :table
+  conn = Faraday.new('https://quietstreamfinancial.github.io') do |f|
+    f.response :json
+  end
+
+  response = conn.get('/eng-recruiting/transactions.json').body
+  users = {}
+
+  response.each do |transaction|
+    user_id = transaction['customer_id']
+    account_type = transaction['account_type']
+    user = users[user_id]
+    transaction_amount = Monetize.parse(transaction['transaction_amount']).to_f
+    account_amount = transaction[account_type].to_f + transaction_amount
+
+    if user.nil?
+      users[user_id] = { account_type => account_amount, 'total' => account_amount, 'name' => transaction['customer_name'] }
+    else
+      user['name'] ||= transaction['customer_name']
+      user[account_type] = account_amount
+      user['total'] = (user['total'] + account_amount).round(2)
+    end
+  end
+
+  haml :table, locals: { data: users, layout: :layout }
 end
